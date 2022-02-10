a12aa80f133bd9f78296d4ed5af58baed17a9613
Mateusz Urbanek <mateusz.urbanek.98@gmail.com>
2022-02-10 16:21:40 +0100

    feat: dynamically get container name

--- configure.sh
+++ configure.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
-echo $1
+CONTAINER=$(docker container ls -aq --filter label='sat.gcs.component=vault')
 
-docker exec $1 vault audit enable file file_path=/vault/logs/dynamic_credential_audit.log log_raw=true
-docker exec $1 vault policy write mongodb-client /vault/logs/mongo-client.hcl
-docker exec $1 vault token create -policy=mongodb-client
-docker exec $1 vault secrets enable database
-docker exec $1 vault write database/config/gy-mongo-demo \
+echo ${CONTAINER}
+
+docker exec ${CONTAINER} vault audit enable file file_path=/vault/logs/dynamic_credential_audit.log log_raw=true
+docker exec ${CONTAINER} vault policy write mongodb-client /vault/logs/mongo-client.hcl
+docker exec ${CONTAINER} vault token create -policy=mongodb-client
+docker exec ${CONTAINER} vault secrets enable database
+docker exec ${CONTAINER} vault write database/config/gy-mongo-demo \
     plugin_name=mongodb-database-plugin \
     allowed_roles="mongo-db-admin" \
     connection_url="mongodb://{{username}}:{{password}}@mongo:27017/admin" \
     username="mongo-admin" \
     password="YWRtaW4tcHc="
-docker exec $1 vault write database/roles/mongo-db-admin \
+docker exec ${CONTAINER} vault write database/roles/mongo-db-admin \
     db_name=gy-mongo-demo \
     creation_statements='{ "db": "admin", "roles": [{ "role": "readWrite" }, {"role": "readWrite", "db": "dev"},{"role": "readWrite", "db": "test"}, {"role": "read", "db": "production"}] }' \
     default_ttl="60s" \
